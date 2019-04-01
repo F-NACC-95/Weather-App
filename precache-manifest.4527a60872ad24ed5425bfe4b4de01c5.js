a2b0ab8d48b34a89086fc5d9fdc8c913dde9a3c9
Franco Naccarato <f-nacc-21@hotmail.com>
2019-04-01 15:32:22 -0400

    Updates

---
+++ precache-manifest.4527a60872ad24ed5425bfe4b4de01c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "fabfdeb46954f8b85ea7",
+    "url": "/weather-app/static/js/main.f360b3f7.chunk.js"
+  },
+  {
+    "revision": "2552e9f7d3e422c9bff7",
+    "url": "/weather-app/static/js/2.63760f30.chunk.js"
+  },
+  {
+    "revision": "fabfdeb46954f8b85ea7",
+    "url": "/weather-app/static/css/main.700a66ce.chunk.css"
+  },
+  {
+    "revision": "3c1bb374b2fa9e4ae8f0afbd289fd530",
+    "url": "/weather-app/index.html"
+  }
+];
