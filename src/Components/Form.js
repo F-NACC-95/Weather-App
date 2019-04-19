cffc2d61b6e41237231412e34950a339288b2c46
Franco Naccarato <f-nacc-21@hotmail.com>
2019-04-19 10:45:20 -0400

    button improvement

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -5,6 +5,7 @@ const Form = props => {
         <form onSubmit={props.getWeather} className="form__component">
             <input type="text" name="city" placeholder="Enter City..."/>
             <input type="text" name="country" placeholder="Enter Country..."/>
+            <br />
             <button className="form__button">Get Weather</button>
         </form>
     );
